248c2a45363109dbcff0fe8e96c4e1d4865af5d3
André LECLERCQ <52776632+AndreLeclercq@users.noreply.github.com>
2020-07-30 09:57:14 +0200

    Delete script1.js

--- js/script1.js
+++
@@ -1,9 +0,0 @@
-
-export let launch = () => console.log('script1 is loaded');
-
-
-export function helloworld(text){
-    document.getElementById('content').textContent = text;
-}
-
-export let msg = (id, txt) => document.getElementById(id).textContent = txt;
